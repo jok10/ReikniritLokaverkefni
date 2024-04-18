85545f38afda29aedf795ce712062bf842c1705a
PannaSigrun <annasg19@ru.is>
2024-04-18 15:32:57 +0000

    Bærri við Node.java

--- src/main/java/hi/reiknirit/Node.java
+++ src/main/java/hi/reiknirit/Node.java
@@ -8,6 +8,12 @@ class Node {
 
     private String id;
     // Other node attributes
+    private String departureTime;
+    private String arrivalTime;
+    private String tripId;
+    private String routeId;
+    private String startStopId;
+    private String endStopId;
     private Map<String, Node> neighbors;
 
     public Node(String id) {
@@ -22,5 +28,29 @@ class Node {
     // Getters and setters
     public String getId() {
         return id;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setTripId(String tripId) {
+        this.tripId = tripId;
+    }
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId;
+    }
+
+    public void setStartStopId(String startStopId) {
+        this.startStopId = startStopId;
+    }
+
+    public void setEndStopId(String endStopId) {
+        this.endStopId = endStopId;
     }
 }
